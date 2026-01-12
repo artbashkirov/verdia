986ef90a5002296b565b00cce0af366329057950
artbashkirov <artbashkirov@users.noreply.github.com>
2026-01-13 01:43:51 +0300

    Настройка кэширования: добавлены заголовки Cache-Control для предотвращения кэширования страниц

--- next.config.ts
+++ next.config.ts
@@ -32,6 +32,19 @@ const nextConfig: NextConfig = {
             key: 'X-XSS-Protection',
             value: '1; mode=block',
           },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+      {
+        source: '/_next/static/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
         ],
       },
     ];
